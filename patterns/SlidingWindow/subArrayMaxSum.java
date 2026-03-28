2f9b0cc82fcc3158e8c67285020b3938ba960cab
Jitu Kumar <jitukumar63766@gmail.com>
2026-03-28 11:19:38 +0530

    unused assigned values removed from the subarraymax sum.

--- patterns/SlidingWindow/subArrayMaxSum.java
+++ patterns/SlidingWindow/subArrayMaxSum.java
@@ -2,21 +2,22 @@ package patterns.SlidingWindow;
 
 public class subArrayMaxSum {
     public static void main(String[] args) {
-        int[] arr = {2, 1, 5, 1, 3, 2};
+        int[] arr = {2, 1, 5, 1, 4, 2};
         int k = 3; // window size
         System.out.println(max(arr, k));
     }
 
     static int max(int[] arr,int k){
-        int maxsum = Integer.MIN_VALUE;
         int windowSum = 0;
 
         for (int i = 0; i < k; i++) {
             windowSum = windowSum + arr[i];
         }
 
+        int maxsum = windowSum; // maxsum = first window size i.e. 2+1+5 = 8
+
         for (int j = k; j < arr.length; j++) {
-            windowSum += arr[j] - arr[j-k];
+            windowSum = windowSum + arr[j] - arr[j-k]; // arr[j-k] = arr[0] first index element removed
             maxsum = Math.max(maxsum, windowSum);
         }
 
